14c540989db4a2434fb290707dea8da875dbba29
John Bostick <john.bostick@gatech.edu>
2025-06-03 12:14:43 -0400

    Create HelloWorld.om.js

---
+++ community/HelloWorld.om.js
@@ -0,0 +1,12 @@
+// This is an example of an OpenMarch plugin that does almost nothing.
+// Copyright (c) 2025 OpenMarch. Licensed under the AGPL-3.0 License.
+
+// Name: Hello World
+// Description: Proves that the plugin system works using the magic of console.log()
+// Version: 1.0.0
+// Author: John Bostick
+
+function HelloWorld() {
+    console.log("The plugin system works!");
+}
+HelloWorld();
